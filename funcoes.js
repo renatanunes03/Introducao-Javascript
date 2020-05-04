ae544909ac2a1f165802f26f9a0d94ce21753480
Renata Santana <renatanunez90@gmail.com>
2020-05-04 15:47:25 -0300

    Javascript

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+var x = 5;
+var y = 0;
+
+/*Python
+if condição:{
+    comandos
+}
+
+elif não existe no JavaScript e sim else if, 
+e sempre colocar apos else do else if.
+
+
+    == Igual
+    != diferente
+    > Maior
+    < menor 
+    <= menor igual
+    >= maior igual
+*/
+
+while(x > 0){
+    alert(x);
+    x++;
+}
